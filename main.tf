bdc13560acc9dae66f2a62d8288f1e212f441c81
pestirA <35427591+pestirA@users.noreply.github.com>
2022-02-13 23:43:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
     hostname     = "app.terraform.io"
     token        = "cxo3MxJ1k0n7dA.atlasv1.ILshRz44PouoMWneKm8fMLFDiuQN5UuzhXbAkaMhr0GTS4YHOvb9tHxa3ccAeEuZyZ8"
     workspaces {
-      name = "tfc-workspace-np"
+      name = "tfc-workspace-prod"
     }
   }
 }
