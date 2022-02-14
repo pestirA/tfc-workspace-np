3d752cd663f41f5972f64a5a5a0c351429e9314d
pestirA <35427591+pestirA@users.noreply.github.com>
2022-02-13 23:10:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
+terraform {
+
+  cloud {
+    organization = "tfc-workspace-np"
+    hostname     = "app.terraform.io"
+    token        = "cxo3MxJ1k0n7dA.atlasv1.ILshRz44PouoMWneKm8fMLFDiuQN5UuzhXbAkaMhr0GTS4YHOvb9tHxa3ccAeEuZyZ8"
+    workspaces {
+      name = "tfc-workspace-np"
+    }
+  }
+}
+
+// Configure the provider
+provider "hcp" {}
+// Use the cloud provider GCP to provision resources that will be connected to HCP
+
+// Use the cloud provider AWS to provision resources that will be connected to HCP
+provider "aws" {
+}
+
+provider "google" {
+}
+
+provider "google-beta" {
+}
 # Declaring the resource block aws_instance
 resource "aws_instance" "my-machine" {
   # Declaring the argument ami
